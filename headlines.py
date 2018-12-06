956d5e49fba7dfdb913e46d9557a92b34191d04f
Sven Bohnstedt <sven.bohnstedt@gmail.com>
2018-12-06 19:36:15 +0100

    route with variable

--- headlines.py
+++ headlines.py
@@ -2,24 +2,26 @@ import feedparser
 from flask import Flask
 import pprint
 
-BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
 
 app = Flask(__name__)
 
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+    'cnn': 'http://rss.cnn.com/rss/edition.rss',
+    'fox': 'http://feeds.foxnews.com/foxnews/latest',
+    'iol': 'http://www.iol.co.za/cmlink/1.640'}
+
 @app.route("/")
-def get_news():
-    feed = feedparser.parse(BBC_FEED)
+@app.route("/<publication>")
+def get_news(publication="bbc"):
+    feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
-    pp = pprint.PrettyPrinter(indent=4, depth=2)
-    pp.pprint(feed)
     return """<html>
     <body>
-    <h1> BBC Headlines </h1>
-    <b>{0}</b> <br/>
-    <i>{1}</i> <br/>
-    <p>{2}</p> <br/>
+    <h1>Headlines </h1>
+    <b>{0}</b> </ br>
+    <i>{1}</i> </ br>
+    <p>{2}</p> </ br>
     </body>
     </html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5000, debug=True)
